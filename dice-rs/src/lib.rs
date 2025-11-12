9aa251d5e8a1ba6d68fa0c672029b1a62f2a9237
Sebastian Strobl <Sebastian@Strobl.net>
2025-11-12 16:51:08 +0100

    Add: safety comments and some documentation
    Signed-off-by: Sebastian Strobl <Sebastian@Strobl.net>

--- dice-rs/src/lib.rs
+++ dice-rs/src/lib.rs
@@ -5,7 +5,10 @@ pub mod raw;
 pub type ChainId = u16;
 pub type TypeId = u16;
 pub type DiceThreadId = u64;
-use std::alloc::{GlobalAlloc, Layout};
+use std::{
+    alloc::{GlobalAlloc, Layout},
+    marker::PhantomData,
+};
 
 #[repr(u16)]
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
@@ -25,11 +28,18 @@ pub trait DiceEvent: Sized {
         None
     }
 
+    /// # Safety
+    /// This function is intended to be used for casting a void pointer from a dice callback to a Rust reference.
+    /// Here we assume that dice correctly returns the pointers to correctly allocated types.
+    /// If the Event does not require any data, dice will not provide a type/struct for this and return a nullpointer
+    /// The `#[dice_event(...)]` attribute macro correctly handles this and implement a fallback.
+    /// The fallback is a &T which is a valid reference for unit structs (structs without a body)
     #[inline]
     unsafe fn from_raw<'a>(ptr: *const ()) -> Option<&'a Self> {
         if ptr.is_null() {
             Self::fallback()
         } else {
+            // SAFETY: we know ptr is not null. and from the dice protocol we know this is also correctly typed.
             Some(unsafe { &*(ptr as *const Self) })
         }
     }
@@ -51,6 +61,8 @@ pub enum DiceResult {
 #[derive(Debug)]
 pub struct Metadata {
     drop_: bool,
+    // This marker makes Metadata !Send and !Sync
+    _marker: PhantomData<*mut ()>,
 }
 
 pub struct MempoolAllocator;
@@ -58,6 +70,8 @@ pub struct MempoolAllocator;
 unsafe impl GlobalAlloc for MempoolAllocator {
     #[inline]
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        // SAFETY: we assume dice allocates correctly
+        // we do an additional sanity check in Debug build to verify this.
         let ptr = unsafe { raw::mempool_alloc(layout.size()) as *mut u8 };
         debug_assert!(
             !ptr.is_null() && (ptr as *mut usize).is_aligned(),
@@ -68,6 +82,8 @@ unsafe impl GlobalAlloc for MempoolAllocator {
 
     #[inline]
     unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
+        // SAFETY: we assume that dice is doing the correct thing.
+        // from rust persepctive this is a black box
         unsafe { raw::mempool_free(ptr as *mut _) };
     }
 }
@@ -79,8 +95,8 @@ pub mod thread {
     use crate::{DiceThreadId, Metadata, raw};
 
     pub fn self_id(mt: &mut Metadata) -> DiceThreadId {
-        let id = unsafe { raw::thread::self_id(mt) };
-        id
+        // SAFETY: we asssume the extern dice is returning the correct id
+        unsafe { raw::thread::self_id(mt) }
     }
 
     // Note: T here does not have to be repr(C) as we do the size calculation on the rust side
@@ -90,19 +106,30 @@ pub mod thread {
         value: MaybeUninit<T>,
     }
 
-    pub struct TlsKey<T: Default> {
+    pub struct TlsKey<T> {
         _marker: PhantomData<T>,
     }
 
-    impl<T: Default> TlsKey<T> {
+    // this is kind of useless, but clippy wants this.
+    impl<T: Default> Default for TlsKey<T> {
+        fn default() -> Self {
+            Self::new()
+        }
+    }
+
+    impl<T> TlsKey<T> {
         pub const fn new() -> Self {
             Self {
                 _marker: PhantomData,
             }
         }
+    }
 
+    impl<T: Default> TlsKey<T> {
         #[inline(always)]
         fn cell_ptr(&self, mt: &mut Metadata) -> *mut TlsCell<T> {
+            // SAFETY: We assume dice correctly returns a pointer for TLS storage.
+            // in debug build we do an additional sanity check that this holds.
             unsafe {
                 let raw = raw::thread::self_tls(
                     mt,
@@ -117,17 +144,28 @@ pub mod thread {
 
         #[inline]
         pub fn with<R>(&self, mt: &mut Metadata, f: impl FnOnce(&mut T) -> R) -> R {
+            // SAFETY: see inside get_mut
             let t = unsafe { self.get_mut(mt) };
             f(t)
         }
 
         #[inline]
+        /// # SAFETY
+        /// this function requires a valid &'a mut Metdata, which forces unique and exclusive usage.
+        /// Furthermore, as Metadata is !Send and !Sync, we also enfornce this thread local object will stay there.
+        /// TODO: we could even consider Pin type.
         pub unsafe fn get_mut<'a>(&self, mt: &'a mut Metadata) -> &'a mut T {
             let cell = self.cell_ptr(mt);
+            // TODO: consider using (#[cold] based) unlikely here as this only happens once
+            // SAFETY: we know the ptr is not null and is correctly aligned from the cell_ptr() call
             if unsafe { !(*cell).initialized } {
+                // SAFETY: external memory, so a volatile write
                 unsafe { std::ptr::write_volatile((*cell).value.as_mut_ptr(), T::default()) };
+                // SAFETY: this is safe from prior checks, set this to true so this won't be called multiple times.
                 unsafe { (*cell).initialized = true };
             }
+            // SAFETY: &'a mut Metadata ensures they have the same lifetime and metadata can only be borrowed once
+            // as metadata is unique per subscribe call and is !Send & !Sync, this is safe.
             unsafe { &mut *(*cell).value.as_mut_ptr() }
         }
     }
@@ -140,10 +178,15 @@ pub mod thread {
     }
 }
 
+/// Create a callback and subscribe it to dice.
+// this subscribe macro emulates a normal rust anonymous function structure.
+// it creates a c callback and subscribes it automatically
+// it is type, lifetime and capture guarded using the _guard
 #[macro_export]
 macro_rules! subscribe_scoped {
     ($chain:expr, $prio:expr, |$e:ident: &$t:ty, $m:ident| $body:block) => {{
-        // no capture guard
+        // this guard enforces no capturing of outside scope variables (except statics of course)
+        // it also enforces lifetimes and types of the $body
         let _guard: fn(&$t, &mut $crate::Metadata) -> $crate::DiceResult =
             |$e: &$t, $m: &mut $crate::Metadata| $body;
 
@@ -153,6 +196,11 @@ macro_rules! subscribe_scoped {
             event: *const core::ffi::c_void,
             md: *mut $crate::Metadata,
         ) -> $crate::DiceResult {
+            // SAFETY: the dice subscribe callback either gives a correctly typed pointer for the event
+            // or it gives a null in case the Event struct is empty.
+            // in this case an empty Fallback is used.
+            // Rust allows to take references of unit types directly and treat them as instances (like &() as () is type fields)
+            // as these have no fields, there is also no concern of possibility of mutating these (potentially shared) references
             let Some(ev_ref) = (unsafe { <$t as $crate::DiceEvent>::from_raw(event as _) }) else {
                 return $crate::DiceResult::Invalid;
             };
@@ -167,6 +215,7 @@ macro_rules! subscribe_scoped {
             $body
         }
 
+        // SAFETY
         unsafe {
             $crate::raw::ps_subscribe(
                 $chain,
@@ -178,6 +227,10 @@ macro_rules! subscribe_scoped {
     }};
 }
 
+/// Create a callback and subscribe it to dice.
+/// this is a convenience macro over `subscribe_scoped` which allows usage in global scope
+/// and subscribes at startup of the program using a startup constructors.
+/// `Once` ensures this only happens once, even in multithreaded applications.
 #[macro_export]
 macro_rules! subscribe {
     ($chain:expr, $slot:expr, |$e:ident: &$t:ty, $m:ident| $body:block) => {
@@ -196,15 +249,18 @@ macro_rules! subscribe {
     };
 }
 
+/// Helper to initialize logging
+/// TODO: make it a struct
 #[macro_export]
 macro_rules! init_dice_state {
-    () => {
+    (log_level: $level:expr) => {
         #[global_allocator]
         static GLOBAL: $crate::MempoolAllocator = $crate::MempoolAllocator;
 
         #[::ctor::ctor]
         fn __init_log() {
-            $crate::log::init(log::LevelFilter::Debug);
+            $crate::log::init($level);
         }
     };
+    () => { init_dice_state!(log_level: $crate::log::LogLevel::Debug); }
 }
