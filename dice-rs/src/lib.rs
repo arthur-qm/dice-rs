d370679df6096c69baf19122b3a3804fa6a3f60a
Sebastian Strobl <Sebastian@Strobl.net>
2025-11-13 13:55:01 +0100

    improve safety comments
    Signed-off-by: Sebastian Strobl <Sebastian@Strobl.net>

--- dice-rs/src/lib.rs
+++ dice-rs/src/lib.rs
@@ -28,19 +28,24 @@ pub trait DiceEvent: Sized {
         None
     }
 
-    /// # Safety
     /// This function is intended to be used for casting a void pointer from a dice callback to a Rust reference.
-    /// Here we assume that dice correctly returns the pointers to correctly allocated types.
     /// If the Event does not require any data, dice will not provide a type/struct for this and return a nullpointer
-    /// The `#[dice_event(...)]` attribute macro correctly handles this and implement a fallback.
+    /// The `#[dice_event(...)]` attribute macro correctly handles this and implements a fallback.
     /// The fallback is a &T which is a valid reference for unit structs (structs without a body)
+    /// # Safety
+    /// usage of a valid pointer to a Self or a nullpointer
+    /// if the type doesn't implement a fallback and the pointer is null, it will return None
     #[inline]
     unsafe fn from_raw<'a>(ptr: *const ()) -> Option<&'a Self> {
         if ptr.is_null() {
             Self::fallback()
         } else {
-            // SAFETY: we know ptr is not null. and from the dice protocol we know this is also correctly typed.
-            Some(unsafe { &*(ptr as *const Self) })
+            let ptr = ptr as *const Self;
+            debug_assert!(ptr.is_aligned(), "Sanity Check");
+
+            // SAFETY: we know ptr has the correct type and is not null.
+            let reference = unsafe { &*ptr };
+            Some(reference)
         }
     }
 }
@@ -56,7 +61,11 @@ pub enum DiceResult {
     Error = -2,
 }
 
-// For now user code is unable to construct this (besides unsafe casting)
+/// Metadata is given in a dice subscribe callback and is constructed in publishers
+/// For example: it is used to access "internal" properties like thread id in Capture chain callbacks.
+/// due to private fields it is not possible to construct Metdata (safely)
+/// it is also !Send and !Sync (from the _marker: PhatnomData<*mut ()>) and no Copy nor Clone.
+/// this is to ensure uniqueness and locality of metadata usage within callbacks.
 #[repr(C, align(8))]
 #[derive(Debug)]
 pub struct Metadata {
@@ -65,26 +74,54 @@ pub struct Metadata {
     _marker: PhantomData<*mut ()>,
 }
 
+/// Allocator backed by dice's Allocator
 pub struct MempoolAllocator;
 
+/// # Safety
+/// 
+/// * the current implementation does unwind, which is UB by definition here. 
+///   This is so we can know if dice is wrong. 
+///   There are no good alternatives, because this means the core C dependency itself is broken.
+/// 
+/// * Dice needs to be patched to use 8 byte alignment. Then less than equal 8 byte alignment works.
+///   everything else from [`std::alloc::GlobalAlloc`] applies 
+///   TODO: update this once dice allows arbitrary alignment or #106 being merged.
+///   we can also consider doing alignment ourselves 
+/// 
+/// * the allocator is from rust's perspective stateless and makes no assumptions about allocations happening.
+///   the rust optimizer is free to not allocate or overallocate.
 unsafe impl GlobalAlloc for MempoolAllocator {
+    
+    /// returns a pointer to correctly sized memory.
+    /// 
+    /// # Errors
+    /// currently the alignment is ignored. but 8 byte alignment can be configured in dice
+    /// 
+    /// # Safety
+    /// to get stable 8 byte alignment, dice needs to be patched: #106 Memory Alignment Patch
+    /// 8 byte alignment seems to be enough for now.
+    /// We do a check here to see if this contract holds, so we can know if we run into requirements we currently cannot guarantee
+    /// this will panic and unwind, which breaks the usual GlobalAlloc contract, 
+    /// but if we abort, we would not get the information we want.
+    /// 
+    /// everything else from [`std::alloc::GlobalAlloc::alloc`] applies
     #[inline]
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        // SAFETY: we assume dice allocates correctly
-        // we do an additional sanity check in Debug build to verify this.
         let ptr = unsafe { raw::mempool_alloc(layout.size()) as *mut u8 };
-        debug_assert!(
-            !ptr.is_null() && (ptr as *mut usize).is_aligned(),
-            "Sanity Check"
+        assert!(
+            !ptr.is_null() && ptr as usize % layout.align() == 0,
+            "Requested alignment {} but got {}", layout.align(), 2 ^ (ptr as usize).trailing_zeros()
         );
         ptr
     }
 
+    /// # Safety
+    /// the current implementation does ignore the layout, but this is a TODO to be changed.
+    /// everything else from [`std::alloc::GlobalAlloc::dealloc`] applies
     #[inline]
     unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
-        // SAFETY: we assume that dice is doing the correct thing.
-        // from rust persepctive this is a black box
-        unsafe { raw::mempool_free(ptr as *mut _) };
+        let ptr = ptr as *mut _;
+        unsafe { raw::mempool_free(ptr) };
     }
 }
 
@@ -94,8 +131,13 @@ pub mod thread {
 
     use crate::{DiceThreadId, Metadata, raw};
 
+    /// get the thread id
+    /// if this is called outside a valid thread it will return 0
+    /// SAFETY:
+    /// requires dice-self
     pub fn self_id(mt: &mut Metadata) -> DiceThreadId {
-        // SAFETY: we asssume the extern dice is returning the correct id
+        // SAFETY: dice will return a number >= 1 if it is a thread, and 0 if it is not within a thread
+        // it requires dice-self
         unsafe { raw::thread::self_id(mt) }
     }
 
@@ -130,21 +172,21 @@ pub mod thread {
         fn cell_ptr(&self, mt: &mut Metadata) -> *mut TlsCell<T> {
             // SAFETY: We assume dice correctly returns a pointer for TLS storage.
             // in debug build we do an additional sanity check that this holds.
-            unsafe {
-                let raw = raw::thread::self_tls(
+            let raw = unsafe {
+                raw::thread::self_tls(
                     mt,
                     self as *const _ as *const _,
                     size_of::<TlsCell<T>>(),
-                );
-                let ptr = raw as *mut TlsCell<T>;
-                debug_assert!(ptr.is_aligned() && !ptr.is_null(), "Sanity Check");
-                ptr
-            }
+                )
+            };
+            let ptr = raw as *mut TlsCell<T>;
+            debug_assert!(ptr.is_aligned() && !ptr.is_null(), "Sanity Check");
+            ptr
         }
 
         #[inline]
         pub fn with<R>(&self, mt: &mut Metadata, f: impl FnOnce(&mut T) -> R) -> R {
-            // SAFETY: see inside get_mut
+            // SAFETY: see the safety contract of `Self::get_mut`
             let t = unsafe { self.get_mut(mt) };
             f(t)
         }
@@ -152,24 +194,25 @@ pub mod thread {
         #[inline]
         /// # SAFETY
         /// this function requires a valid &'a mut Metdata, which forces unique and exclusive usage.
-        /// Furthermore, as Metadata is !Send and !Sync, we also enfornce this thread local object will stay there.
-        /// TODO: we could even consider Pin type.
+        /// Furthermore, as Metadata is !Send and !Sync, we also enforce that this thread local object will stay there.
+        /// TODO: we could even consider `Pin` type.
         pub unsafe fn get_mut<'a>(&self, mt: &'a mut Metadata) -> &'a mut T {
             let cell = self.cell_ptr(mt);
-            // TODO: consider using (#[cold] based) unlikely here as this only happens once
-            // SAFETY: we know the ptr is not null and is correctly aligned from the cell_ptr() call
-            if unsafe { !(*cell).initialized } {
-                // SAFETY: external memory, so a volatile write
-                unsafe { std::ptr::write_volatile((*cell).value.as_mut_ptr(), T::default()) };
-                // SAFETY: this is safe from prior checks, set this to true so this won't be called multiple times.
-                unsafe { (*cell).initialized = true };
-            }
             // SAFETY: &'a mut Metadata ensures they have the same lifetime and metadata can only be borrowed once
             // as metadata is unique per subscribe call and is !Send & !Sync, this is safe.
-            unsafe { &mut *(*cell).value.as_mut_ptr() }
+            let cell = unsafe { &mut *cell };
+            // TODO: consider using (#[cold] based) unlikely here as this only happens once
+            if !cell.initialized {
+                cell.initialized = true;
+                // SAFETY: external memory, so a volatile write
+                unsafe { std::ptr::write_volatile((*cell).value.as_mut_ptr(), T::default()) };                
+            }
+            // SAFETY: this value is initialized now
+            unsafe { cell.value.assume_init_mut() }
         }
     }
 
+    /// Create a key for a dice thread local allocation with the same type.
     #[macro_export]
     macro_rules! tls_key {
         ($name:ident : $ty:ty) => {
@@ -178,7 +221,7 @@ pub mod thread {
     }
 }
 
-/// Create a callback and subscribe it to dice.
+/// Create a callback and subscribe to dice.
 // this subscribe macro emulates a normal rust anonymous function structure.
 // it creates a c callback and subscribes it automatically
 // it is type, lifetime and capture guarded using the _guard
